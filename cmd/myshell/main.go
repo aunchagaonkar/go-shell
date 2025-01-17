442ef382e5ea3a9f06cbf93b7d681249d8eef6f1
aunchagaonkar <ameya21unchagaonkar@gmail.com>
2025-01-17 13:37:53 +0530

    while true

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -12,15 +12,16 @@ var _ = fmt.Fprint
 
 func main() {
 	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
-
-	// Wait for user input
-	command , err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		// Wait for user input
+		command , err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+		}
+	
+		command = strings.TrimSpace(command)
+		fmt.Printf("%s: command not found\n", command)
 	}
-
-	command = strings.TrimSpace(command)
-	fmt.Printf("%s: command not found\n", command)
 
 }
